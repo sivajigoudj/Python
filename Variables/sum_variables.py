ea1b89266e505ff99f9bcca7a2cc95501a5dcd11
sivajigoudj <jsivajigoud9412@gmail.com>
2025-02-23 11:17:55 +0530

    Declare two variables a = 5 and b = 10, then print their sum

---
+++ Variables/sum_variables.py
@@ -0,0 +1,10 @@
+#Declare two variables a = 5 and b = 10, then print their sum
+
+a = 5
+b = 10
+
+# Calculate the sum
+sum_result = a + b
+
+# Print the result
+print("The sum of a and b is:", sum_result)
